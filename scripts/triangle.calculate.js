f32a3a5a8fb534c19ef226b808d976caeb1a1733
Tanvir Ahmad <tnvir9@gmail.com>
2024-02-13 00:09:36 +0600

    updated triangle section with js

--- scripts/triangle.calculate.js
+++ scripts/triangle.calculate.js
@@ -1,3 +1,15 @@
 function triangleAreaCalculate() {
-    console.log('connected');
+    const getBaseInput = document.getElementById('get-base-input');
+    const getHeightInput = document.getElementById('get-height-input');
+
+    const getBaseInputValue = getBaseInput.value;
+    const getHeightInputValue = getHeightInput.value;
+    //console.log(typeof getBaseInputValue); //* output is string now.But string is not allowed. 
+    //console.log(typeof getHeightInputValue); //* output is string now.But string is not allowed.
+
+    const getBaseInputValueInNumber = parseFloat(getBaseInputValue); //* using parseFloat because of value can be fraction number(1.5) 
+    const getHeightHeightValueInNumber = parseFloat(getHeightInputValue); //* using parseFloat because of value can be fraction number(1.5) 
+
+    const totalArea = 0.5 * getBaseInputValueInNumber * getHeightHeightValueInNumber;
+    console.log(totalArea);
 }
